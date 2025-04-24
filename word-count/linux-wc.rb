c0b3fbe465ce2c48f5ac8213465e3a26408fba15
Manish Kumar Sahu <msahu300@gmail.com>
2025-04-24 13:15:33 +0530

    Added support to read from standard input

--- word-count/linux-wc.rb
+++ word-count/linux-wc.rb
@@ -1,37 +1,25 @@
 #!/usr/bin/env ruby
 
+# Implements word count functionality similar to Unix wc command
 class LinuxWc
-  def initialize(file_path)
-    @file_path = file_path
-  end
-
-  def read_file
-    if File.exist?(@file_path)
-      File.read(@file_path)
-    else
-      puts "Error: File '#{@file_path}' does not exist"
-      exit 1
-    end
-  end
-
-  def file_name
-    @file_path
+  def initialize(content)
+    @content = content
   end
 
   def count_bytes
-    read_file.bytesize
+    @content.bytesize
   end
 
   def count_lines
-    read_file.count("\n")
+    @content.count("\n")
   end
 
   def count_words
-    read_file.scan(/\S+/).count
+    @content.scan(/\S+/).count
   end
 
   def count_characters
-    read_file.chars.count
+    @content.chars.count
   end
 end
 
@@ -40,22 +28,57 @@ if ARGV.empty?
   exit 1
 end
 
+# Read content from file path then do word count functionality
+class LinuxWcWithFilePath < LinuxWc
+  def initialize(file_path)
+    unless File.exist?(file_path)
+      puts "Error: File '#{file_path}' does not exist"
+      exit 1
+    end
+
+    content = File.read(file_path)
+    super(content)
+    @file_path = file_path
+  end
+
+  def file_name
+    @file_path
+  end
+end
+
+def execute_wc(wc)
+  if ARGV.include?('-c')
+    puts wc.count_bytes
+  elsif ARGV.include?('-l')
+    puts wc.count_lines
+  elsif ARGV.include?('-w')
+    puts wc.count_words
+  elsif ARGV.include?('-m')
+    puts wc.count_characters
+  else
+    puts 'invalid flag'
+    exit 1
+  end
+end
+
+# file content is taken from terminal
+# eg cat ./word-count/test.txt | ccwc -l
+unless $stdin.tty?
+  wc = LinuxWc.new($stdin.read)
+  execute_wc(wc)
+
+  exit 0
+end
+
+# no flags
+# eg ccwc -m ./word-count/test.txt
 if ARGV.length == 1
-  wc = LinuxWc.new(ARGV[0])
+  wc = LinuxWcWithFilePath.new(ARGV[0])
   puts "    #{wc.count_lines}   #{wc.count_words}   #{wc.count_bytes}   #{wc.file_name}"
   exit 0
 end
 
-wc = LinuxWc.new(ARGV[1])
-if ARGV.include?('-c')
-  puts wc.count_bytes
-elsif ARGV.include?('-l')
-  puts wc.count_lines
-elsif ARGV.include?('-w')
-  puts wc.count_words
-elsif ARGV.include?('-m')
-  puts wc.count_characters
-else
-  puts 'invalid flag'
-  exit 1
-end
+# with flags
+# eg ccwc ./word-count/test.txt
+wc = LinuxWcWithFilePath.new(ARGV[1])
+execute_wc(wc)
