afaea32a1be98ea8246f85556716a51bdc337865
Manish Kumar Sahu <msahu300@gmail.com>
2025-04-25 22:52:26 +0530

    Refactor JSON parser to utilize regex for pattern matching, enhancing key and value validation. Implemented a new method for pattern detection and improved error handling for invalid keys and values.

--- json-parser/parser.rb
+++ json-parser/parser.rb
@@ -1,13 +1,44 @@
 # frozen_string_literal: true
 
+# https://regex101.com/r/1UUUxr/1
+# TODO: add support to check values which are arrays, objects, strings, numbers, booleans, null
 # Parses the provided JSON file to check if its valid
 class JsonParser
   attr_accessor :tokens, :result, :json
+
+  OPEN_BRACE_REGEX =  /\{/.freeze
+  CLOSE_BRACE_REGEX = /}/.freeze
+  SEMI_COLON_REGEX = /:/.freeze
+  COMMA_REGEX = /,/.freeze
+  KEY_PAIR_REGEX = /["']?[\w\s_-]+["']?\s?:/.freeze
+  STRING_REGEX = /"[^"]*"/.freeze
+  NUMBER_REGEX = /-?\d+(?:\.\d+)?/.freeze
+  BOOLEAN_REGEX = /true|false/.freeze
+  NULL_REGEX = /null/.freeze
+  VALUE_REGEX = /:\s?["']?[\w\s_-]+["']?/.freeze
+  VALID_VALUES = /-?\d+(?:\.\d+)?|true|false|null|["']?[\w\s_-]+["']/.freeze
+  INVALID_COMMAS = /,\s?\}/.freeze
 
   def initialize(json)
     @json = json
     @tokens = { close_braces: 0, open_braces: 0, commas: 0, semi_colons: 0, keys: [], values: [] }
     @result = json.to_s
+  end
+
+  def find_pattern
+    open_braces = @result.scan(OPEN_BRACE_REGEX)
+    close_braces = @result.scan(CLOSE_BRACE_REGEX)
+    commas = @result.scan(COMMA_REGEX)
+    semi_colons = @result.scan(SEMI_COLON_REGEX)
+    keys = @result.scan(KEY_PAIR_REGEX)
+    values = @result.scan(VALUE_REGEX)
+
+    @tokens[:open_braces] = open_braces.length
+    @tokens[:close_braces] = close_braces.length
+    @tokens[:commas] = commas.length
+    @tokens[:semi_colons] = semi_colons.length
+    @tokens[:keys] = keys
+    @tokens[:values] = values
   end
 
   def find_and_add(identifier, update_to)
@@ -16,23 +47,9 @@ class JsonParser
     end
   end
 
-  def find_keys_values
-    @result.split(',') do |word|
-      word.split(':') do |key_value, index|
-        index.even ? @tokens[:keys].push(key_value) : @tokens[:values].push(key_value)
-      end
-    end
-  end
-
   def lexer
-    find_and_add('{', :open_braces)
-    find_and_add('}', :close_braces)
-    find_and_add(',', :commas)
-    find_and_add(':', :semi_colons)
-
-    return unless braces
-
-    find_keys_values
+    find_pattern
+    nil unless braces
   end
 
   def braces
@@ -50,7 +67,7 @@ class JsonParser
   def commas
     return unless @tokens[:commas].positive?
 
-    return if @tokens[:semi_colons] - 1 == @tokens[:commas]
+    return if @result.scan(INVALID_COMMAS).empty?
 
     puts 'Invalid JSON, got extra comma'
     exit 1
@@ -60,8 +77,19 @@ class JsonParser
     return unless @tokens[:keys].length.positive?
 
     @tokens[:keys].each do |key|
-      unless key.is_a?(String)
-        puts 'key is not a string'
+      unless key.match(STRING_REGEX)
+        puts "Invalid key #{key}"
+        exit 1
+      end
+    end
+  end
+
+  def values
+    return unless @tokens[:values].length.positive?
+
+    @tokens[:values].each do |value|
+      unless value.match(VALID_VALUES)
+        puts "Invalid value #{value}"
         exit 1
       end
     end
@@ -71,6 +99,7 @@ class JsonParser
     braces
     commas
     keys
+    values
     puts 'Valid JSON'
   end
 
@@ -80,7 +109,4 @@ class JsonParser
   end
 end
 
-jsonFile = File.read(ARGV[0])
-puts jsonFile
-
-JsonParser.new(jsonFile).check
+JsonParser.new(File.read(ARGV[0])).check
